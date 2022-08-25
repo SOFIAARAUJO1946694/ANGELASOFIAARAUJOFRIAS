4847cb4c1a68e5d765d2de605364f870ae6cde23
SOFIAARAUJO1946694 <111946926+SOFIAARAUJO1946694@users.noreply.github.com>
2022-08-25 12:59:23 -0500

    Add files via upload

---
+++ TAREA 2 DIVISION.py
@@ -0,0 +1,19 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Hola, esto funciona para realziar divisiones")
+	print("Ingresa tu primer numero a dividir, recuerda utilizar enter despues de introducir tu numero")
+	numa = float(input())
+	print("Ingresa tu segundo numero a dividir, recuerda utilizar enter despues de introducir tu numero")
+	numb = float(input())
+	numc = numa/numb
+	print(numc," Es la respuesta")
+	import os
+	os.system("pause")
+	
+	
+
+        
